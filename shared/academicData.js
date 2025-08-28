e74d5955ff9a9071c13231b24568a3f46e75b95a
Badmus2005 <tchidehoudodjivirgile@gmail.com>
2025-08-28 22:43:14 +0100

    Changement

--- shared/academicData.js
+++ shared/academicData.js
@@ -1,10 +1,33 @@
 // shared/academicData.js
 export const ACADEMIC = {
     ECOLES: {
-        EGEI: ['Électronique', 'Génie Télécoms et TIC', 'Informatique Industrielle', 'Electrotechnique'],
-        ESMEA: ['Assurances', 'Banque', 'Audit', 'RH', 'Commerce', 'Logistique'],
-        FSAE: ['Environnement', 'Ressources Animales', 'Production Végétale'],
-        FDE: ['Droit', 'Économie']
+        EGEI: [
+            'Électronique',
+            'Génie Télécoms et TIC',
+            'Informatique Industrielle et Maintenance',
+            'Electrotechnique'],
+
+        ESMEA: [
+            'Assurances',
+            'Banque et Finance d\'Entreprise',
+            'Audit et Contrôle de Gestion',
+            'Management des Ressources Humaines',
+            'Action Commerciale et Force de Vente',
+            'Communication et Action Publicitaire',
+            'Commerce',
+            'Informatique de Gestion',
+            'Transport et Logistique'],
+
+        FSAE: [
+            'Gestion de l\'Environnement et Aménagement du Territoire',
+            'Production et Gestion des Ressources Animales',
+            'Sciences et Techniques de Production Végétale',
+            'Stockage Conservation et Conditionnement des Produits Agricoles',
+            'Gestion des Entreprises Rurales et Agricoles'],
+
+        FDE: [
+            'Droit',
+            'Economie']
     },
     ANNEES: [1, 2, 3]
 };
